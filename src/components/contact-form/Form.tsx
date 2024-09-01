0f553a7465ef24162a5d5c15c441f3bbc2045179
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-09-01 02:49:56 -0700

    feat: added submit button
    TODO: change color value on hover

--- src/components/contact-form/Form.tsx
+++ src/components/contact-form/Form.tsx
@@ -7,21 +7,32 @@ import { Box, Text, Input, Textarea, Button } from "@chakra-ui/react";
 
 export function Form() {
     const {register, handleSubmit} = useForm();
-    const [data, setData] = useState("");
-    const inputMargins = 30
+    //const [data, setData] = useState();
+    const inputMargins = 1
+    const submitForm = ((data: any) => {
+        console.log(data) // EMAIL LOGIC HERE
+    })  
+    
 
     return (
         <Box 
             borderRadius='10px' 
             width='40%' 
-            margin='100px' 
+            margin='auto' 
             backgroundColor='accent'
             p={10}>
-            <Text>To: <span><Text>ubccookingclubinfo@gmail.com</Text></span></Text>
-            <Input size='md' placeholder="Email" />
-            <Input size='md' placeholder="Subject" />
-            <Textarea size='md' placeholder="Your message" height={200}/>
-
+            <form onSubmit={handleSubmit(submitForm)}>
+                <Text>To: <span>ubccookingclubinfo@gmail.com</span></Text>
+                <Input size='md' placeholder="Email" type="email" margin={inputMargins} {...register("Email", {required: true})} />
+                <Input size='md' placeholder="Subject" margin={inputMargins} {...register("Subject", {required: true})}/>
+                <Textarea size='md' placeholder="Your message" height={200} margin={inputMargins} {...register("Your message", {required: true})}/>
+                <Button type="submit" 
+                borderRadius='20px' 
+                backgroundColor='primary' 
+                color='background' 
+                width='100%'
+                margin={inputMargins}>Send to the Kitchen!</Button>
+            </form>
         </Box>
     )
 }
