c3df668683a38470183bac9472cdeee4ee291227
Matthew Haryanto <matthewanh@hotmail.com>
2024-09-10 12:52:24 -0700

    feat: add join us btn

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,20 +1,29 @@
 "use client";
 
 import Image from "@/components/Image";
-import { Flex, IconButton, Link, useBreakpointValue } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  IconButton,
+  Link,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useState } from "react";
 import Hamburger from "./Hamburger";
 import Menu from "./Menu";
+import JoinBtn from "./JoinBtn";
 
 export default function Navbar() {
   const [active, setActive] = useState<boolean>(false);
-  const isMobile = useBreakpointValue({ xs: true, sm: false });
+  const isMobile = useBreakpointValue({ base: true, md: false });
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} py={1} px={6}>
       <Image src={"/logo.svg"} width={16} height={16} alt={"img"} />
+      {/* Empty box to center navlinks */}
+      <Box />
       {!isMobile && (
-        <Flex justifyContent={"space-around"} w={500}>
+        <Flex justifyContent={"space-around"} w={{ md: 400, lg: 500 }}>
           <Link as={NextLink} href="">
             home
           </Link>
@@ -29,13 +38,20 @@ export default function Navbar() {
           </Link>
         </Flex>
       )}
-      <IconButton
-        variant="unstyled"
-        aria-label="hamburger"
-        background={"transparent"}
-        icon={<Hamburger active={active} />}
-        onClick={() => setActive(!active)}
-      />
+      <Flex
+        width={"20%"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        {!isMobile && <JoinBtn />}
+        <IconButton
+          variant="unstyled"
+          aria-label="hamburger"
+          background={"transparent"}
+          icon={<Hamburger active={active} />}
+          onClick={() => setActive(!active)}
+        />
+      </Flex>
       {active && <Menu />}
     </Flex>
   );
