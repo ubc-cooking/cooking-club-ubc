611bf72b1ef0f3d10c91c195194020cd5c79c707
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-09-17 01:17:03 -0700

    feat: added email functionality
    used web3forms for email functionality
    
    added react-hcaptcha package

--- src/components/contact-form/Form.tsx
+++ src/components/contact-form/Form.tsx
@@ -3,13 +3,36 @@
 import { useForm } from "react-hook-form";
 import { Box, Text, Input, Textarea, Button, Flex } from "@chakra-ui/react";
 import { FaArrowTurnUp } from "react-icons/fa6";
+import HCaptcha from "@hcaptcha/react-hcaptcha";
 
 export function Form() {
-    const {register, handleSubmit} = useForm();
-    const inputMargins = 1
-    const submitForm = ((data: any) => {
-        console.log(data) // EMAIL LOGIC HERE
-    })  
+    const {register, handleSubmit, setValue} = useForm();
+    const inputMargins = 1      // margins for the inputs
+
+    // for captcha
+    const onHCaptchaChange = (token: any) => {
+        setValue("h-captcha-response", token);
+      };
+
+    // web3forms submit
+    async function submitForm(data: any) {
+        data.access_key = "ACCESS_KEY_HERE"
+        const jsonData = JSON.stringify(data)
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: jsonData,
+        });
+        
+        const result = await response.json();
+        if (result.success) {
+            // on success
+        }
+    }  
     
 
     return (
@@ -28,10 +51,19 @@ export function Form() {
                         <Text fontWeight="bold">To:&nbsp;</Text>
                         <Text bg='#7BB1DE' padding='5px 10px' borderRadius='7px' as='u' margin={inputMargins}>ubccookingclubinfo@gmail.com</Text>
                     </Flex>
+                    
+                    <Input size='md' placeholder="Name" margin={inputMargins} outlineColor='accent' {...register("name", {required: true})}/>
                     <Input size='md' placeholder="Email" type="email" margin={inputMargins} outlineColor='accent' {...register("email", {required: true})} />
-                    <Input size='md' placeholder="Subject" margin={inputMargins} outlineColor='accent' {...register("subject", {required: true})}/>
-                    <Textarea size='md' placeholder="Your message" height={180} margin={inputMargins} outlineColor='accent' {...register("message", {required: true})}/>
+                    <Textarea size='md' placeholder="Message" height={180} margin={inputMargins} outlineColor='accent' {...register("message", {required: true})}/>
+                    <Flex flexDirection='column' placeContent='center'>
+                        <HCaptcha
+                            sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+                            reCaptchaCompat={false}
+                            onVerify={onHCaptchaChange}
+                        />
+                    </Flex>
                 </Box>
+
                 <Button type="submit" 
                 borderRadius='20px' 
                 backgroundColor='primary' 
