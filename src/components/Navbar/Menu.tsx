54ee6264783a45a46dae3a9511f38fcc51ec481d
Matthew Haryanto <matthewanh@hotmail.com>
2024-09-16 19:28:28 -0700

    feat: add socials

--- src/components/Navbar/Menu.tsx
+++ src/components/Navbar/Menu.tsx
@@ -1,15 +1,23 @@
+import Image from "@/components/Image";
 import {
   Box,
   Center,
   Divider,
   Flex,
   Heading,
+  Icon,
   Link,
   Text,
 } from "@chakra-ui/react";
-import Image from "@/components/Image";
-5;
 import NextLink from "next/link";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTiktok,
+} from "react-icons/fa";
+import JoinBtn from "./JoinBtn";
+5;
 
 export default function Menu() {
   return (
@@ -21,7 +29,7 @@ export default function Menu() {
       width={"100%"}
       zIndex={20}
       background={"primary"}
-      px={40}
+      px={{ base: 5, sm: 20, md: 40 }}
     >
       <Center>
         <Image src="/dark-logo.svg" width={20} height={20} alt="" mt={3} />
@@ -32,13 +40,55 @@ export default function Menu() {
       <Flex flexDir={"column"}>
         {["HOME.", "ABOUT.", "RECIPES.", "CONTACT."].map((link) => (
           <Link key={link} as={NextLink} href="/" my={2}>
-            <Heading color={"background"} fontSize={"7xl"} mb={4}>
+            <Heading
+              color={"background"}
+              fontSize={{ base: "5xl", md: "7xl" }}
+              mb={4}
+            >
               {link}
             </Heading>
             <Divider borderColor={"background"} borderWidth={"2px"} />
           </Link>
         ))}
       </Flex>
+      <Center>
+        <Flex
+          position={"absolute"}
+          flexDir={"row"}
+          width={{ base: "90%", sm: "70%" }}
+          bottom={"7%"}
+          justifyContent={"space-between"}
+          zIndex={500}
+        >
+          <JoinBtn />
+          <Flex
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            width={{ base: "50%", md: "20%" }}
+          >
+            <Icon
+              as={FaInstagram}
+              boxSize={{ base: 5, md: 7 }}
+              color={"background"}
+            />
+            <Icon
+              as={FaTiktok}
+              boxSize={{ base: 5, md: 7 }}
+              color={"background"}
+            />
+            <Icon
+              as={FaFacebookF}
+              boxSize={{ base: 5, md: 7 }}
+              color={"background"}
+            />
+            <Icon
+              as={FaLinkedinIn}
+              boxSize={{ base: 5, md: 7 }}
+              color={"background"}
+            />
+          </Flex>
+        </Flex>
+      </Center>
     </Box>
   );
 }
