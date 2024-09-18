0bacb9e7a8d0897acdb2e2a383b53305efe92c37
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-09-17 22:45:04 -0700

    feat: created branch for Upcoming Events component

---
+++ src/components/UpcomingEvent/UpcomingEvent.tsx
@@ -0,0 +1,12 @@
+import { ChakraProvider, Flex, Box } from "@chakra-ui/react";
+import theme from "../../theme/index";
+
+export function LatestEvent() {
+    return (
+        <ChakraProvider theme={theme}>
+            <Box backgroundColor='accent'>
+                
+            </Box>
+        </ChakraProvider>
+    )
+}
