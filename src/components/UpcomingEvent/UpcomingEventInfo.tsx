d54bf8db66de8045e48e81105454918d308a6aa5
cheesebricked <131835612+cheesebricked@users.noreply.github.com>
2024-09-19 11:07:18 -0700

    feat: added more to the text part of the UpcomingEvent component

--- src/components/UpcomingEvent/UpcomingEventInfo.tsx
+++ src/components/UpcomingEvent/UpcomingEventInfo.tsx
@@ -1,4 +1,6 @@
-import { Box, Heading, Flex, Text } from "@chakra-ui/react"
+"use client";
+
+import { Box, Heading, Flex, Text, Button } from "@chakra-ui/react"
 
 export function UpcomingEventInfo({
     title = "Title of event",
@@ -6,23 +8,38 @@ export function UpcomingEventInfo({
     where = "Hells kitchen",
     when = "March 23, 2030 10:30am-12:30pm"}) {
 
-    const textMargin = 3
-
+    const textMargin = 4
+    const fontSizeBody = 14
+    const handleSubmit = () => {
+        // link to google form
+    }
 
     return (
-        <Flex backgroundColor='accent' width="24%" borderRadius='5px' padding='30px' flexDirection='column'>
+        <Flex backgroundColor='accent' width="28%" borderRadius='5px' padding='30px 50px' flexDirection='column'>
             <Heading marginBottom={textMargin}>
                 {title}
             </Heading>
-            <Text marginBottom={textMargin}>
+            <Text marginBottom={textMargin} fontSize={fontSizeBody}>
                 {body}
             </Text>
-            <Text marginBottom={textMargin}>
-                {where}
+            <Flex marginBottom={1} flexDirection='row'>
+                <Text as='b' fontSize={fontSizeBody}>Where:&nbsp;</Text>
+                <Text fontSize={fontSizeBody}>{where}</Text>
+            </Flex>
+            <Flex marginBottom={textMargin} flexDirection='row'>
+                <Text as='b' fontSize={fontSizeBody}>When:&nbsp;</Text>
+                <Text fontSize={fontSizeBody}>{when}</Text>
+            </Flex>
+            <Text marginBottom={1} as='b' fontSize={fontSizeBody}>
+                Got Questions?
             </Text>
-            <Text marginBottom={textMargin}>
-                {when}
-            </Text>
+            <Flex marginBottom={textMargin} flexDirection='row'>
+                <Text fontSize={fontSizeBody}>Contact us at&nbsp;</Text>
+                <Text as='u' fontSize={fontSizeBody}>ubccookingclubinfo@gmail.com</Text>
+            </Flex>
+            <Button onSubmit={handleSubmit} width='30%' backgroundColor='secondary' color='background'>
+                Register
+            </Button>
         </Flex>
     )
 }
