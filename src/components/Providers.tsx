916f2b4e2fd691ef55af606a976848b57b9ce6e5
Matthew Haryanto <matthewanh@hotmail.com>
2024-10-27 20:29:25 -0700

    chore(deps): migrate chakra and next

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
-import theme from "@/theme";
+import { system } from "@/theme";
 
 export default function RootProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return <ChakraProvider value={system}>{children}</ChakraProvider>;
 }
