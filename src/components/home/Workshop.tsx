c847f4f1a944875276eb1dcf18a998201b8f1ac9
Matthew Haryanto <matthewanh@hotmail.com>
2024-11-12 13:58:14 -0800

    feat: add bg image and cta

--- src/components/home/Workshop.tsx
+++ src/components/home/Workshop.tsx
@@ -1,21 +1,34 @@
 "use client";
 
-import { Box, Heading, Text, chakra, Center } from "@chakra-ui/react";
+import { Box, Heading, Text, chakra, Center, Button } from "@chakra-ui/react";
 import Image from "../Image";
+import NextLink from "next/link";
 
 export default function Workshop() {
   return (
     <Box position={"relative"} w={"100%"} h={"30rem"}>
       <Image
-        src={"/home/placeholder.webp"}
+        src={"/home/workshop.webp"}
+        // bgGradient={"linear(to-b, url(/home/workshop.webp), pink.500)"}
+        zIndex={-1}
+        opacity={"30%"}
         alt="background"
         width={"200"}
         height={"200"}
         h={"100%"}
         w={"100%"}
-        objectFit={"contain"}
+        objectFit={"cover"}
         position={"absolute"}
         top={0}
+      />
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        // bgGradient="linear(to-t, rgba(255, 249, 225, 1) 2%, rgba(255, 255, 255, 0) 90%)"
+        zIndex="1"
       />
       <Center height={"100%"}>
         <Box textAlign={"center"}>
@@ -33,6 +46,23 @@ export default function Workshop() {
             DON&apos;T WORRY CHECK OUT OUR{" "}
             <chakra.span fontFamily={"heading"}>RECIPES</chakra.span> PAGE
           </Text>
+          <Button
+            size={"lg"}
+            variant={"outline"}
+            borderColor={"secondary"}
+            color={"secondary"}
+            borderRadius={"full"}
+            fontFamily={"heading"}
+            fontWeight={"regular"}
+            border={"2px"}
+            mt={8}
+            _hover={{
+              background: "secondary",
+              color: "background",
+            }}
+          >
+            RECIPES
+          </Button>
         </Box>
       </Center>
     </Box>
