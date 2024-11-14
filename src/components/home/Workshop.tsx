cc1b9e549cf4e319e4666fb96815dfe1499ca07b
Matthew Haryanto <matthewanh@hotmail.com>
2024-11-14 12:29:43 -0800

    feat: add gradient

--- src/components/home/Workshop.tsx
+++ src/components/home/Workshop.tsx
@@ -27,10 +27,19 @@ export default function Workshop() {
         left="0"
         width="100%"
         height="100%"
-        // bgGradient="linear(to-t, rgba(255, 249, 225, 1) 2%, rgba(255, 255, 255, 0) 90%)"
-        zIndex="1"
+        bgGradient="linear(to-b, rgba(255, 249, 225, 1) 0%, rgba(255, 255, 255, 0) 20%)"
+        zIndex={1}
       />
-      <Center height={"100%"}>
+      <Box
+        position="absolute"
+        top="0"
+        left="0"
+        width="100%"
+        height="100%"
+        bgGradient="linear(to-t, rgba(255, 249, 225, 1) 0%, rgba(255, 255, 255, 0) 20%)"
+        zIndex={1}
+      />
+      <Center height={"100%"} position={"relative"} zIndex={10}>
         <Box textAlign={"center"}>
           <Heading fontSize={"5xl"}>
             <chakra.span
