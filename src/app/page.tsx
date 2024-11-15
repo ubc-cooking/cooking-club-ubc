fdc62c27f07074e38c2be380bec083726b58d11f
Matthew Haryanto <matthewanh@hotmail.com>
2024-11-15 00:09:50 -0800

    feat: add faq section temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FAQ from "@/components/home/FAQ";
 import Workshop from "@/components/home/Workshop";
 import { Box } from "@chakra-ui/react";
 import { Contact } from "@/components/contact-form/ContactForm";
@@ -5,7 +6,9 @@ import { Contact } from "@/components/contact-form/ContactForm";
 export default function Home() {
   return (
     <Box>
+      <Box width={"100%"} height={"100vh"} />
       <Workshop />
+      <FAQ />
     </Box>
   );
 }
