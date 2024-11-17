309bce93e092aeefb534b650e1ff777c11097147
Matthew Haryanto <matthewanh@hotmail.com>
2024-11-17 15:48:17 -0800

    feat: gallery layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,19 @@
+import Gallery from "@/components/Gallery";
+import { Box, Heading } from "@chakra-ui/react";
+
 export default function About() {
-  return <></>;
+  return (
+    <Box height={"fit-content"}>
+      <Heading
+        color={"primary"}
+        fontSize={"6xl"}
+        mt={40}
+        w={"100%"}
+        textAlign={"center"}
+      >
+        Get To Know More!
+      </Heading>
+      <Gallery />
+    </Box>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body style={{ minHeight: "fit-content" }}>
+      <body>
         <RootProviders>
           
           <Navbar />
