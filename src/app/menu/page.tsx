22b8413c831f8a2cd1fe434e750fc8b5bbadba47
Matthew Haryanto <matthewanh@hotmail.com>
2024-12-25 16:56:10 -0800

    feat: add title

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,10 +1,28 @@
+import Image from "@/components/Image";
 import Recipes from "@/components/menu/Recipes";
-import { Box, Container } from "@chakra-ui/react";
+import { Box, Container, Heading } from "@chakra-ui/react";
 
 export default function MenuPage() {
   return (
     <Container maxW={"7xl"}>
-      <Box height={"100vh"} />
+      <Box height={"20vh"} />
+      <Heading
+        fontFamily={"body"}
+        mb={5}
+        position={"relative"}
+        width={"fit-content"}
+      >
+        Latest Recipes
+        <Image
+          src={"/menu/menuIcon.svg"}
+          alt="menu"
+          width={10}
+          height={10}
+          position={"absolute"}
+          top={"-1.5rem"}
+          right={0}
+        />
+      </Heading>
       <Recipes />
     </Container>
   );
