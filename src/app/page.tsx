5adc46040368418249430eaaa497af54a437ba8a
Matthew Haryanto <matthewanh@hotmail.com>
2024-12-27 15:52:47 -0800

    feat: add video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FAQ from "@/components/home/FAQ";
+import Landing from "@/components/home/Landing";
 import Workshop from "@/components/home/Workshop";
 import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Box>
-      <Box width={"100%"} height={"100vh"} />
+    <Box position={"relative"}>
+      <Landing />
+      <Box height="10vh"></Box>
       <Workshop />
       <FAQ />
     </Box>
---
+++ src/components/home/Landing.tsx
@@ -0,0 +1,20 @@
+import { AspectRatio, Box } from "@chakra-ui/react";
+
+export default function Landing() {
+  return (
+    <Box position={"relative"} width={"100%"} height={"100vh"}>
+      <AspectRatio maxW="100%" objectFit={"cover"} height={"100%"}>
+        <video autoPlay muted loop id="myVideo" src="/home/landing.mp4">
+          Your browser does not support HTML5 video.
+        </video>
+      </AspectRatio>
+      <Box
+        position={"absolute"}
+        top={0}
+        width={"100%"}
+        height={"100%"}
+        background={"rgb(255,249,225,30%)"}
+      />
+    </Box>
+  );
+}
