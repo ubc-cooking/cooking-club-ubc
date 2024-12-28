48f667228113df4aee34732d6c1ab408a573938f
Matthew Haryanto <matthewanh@hotmail.com>
2024-12-28 01:47:25 -0800

    feat: add current event

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FAQ from "@/components/home/FAQ";
 import Workshop from "@/components/home/Workshop";
+import Event from "@/components/home/Event";
 import { Box } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Box>
       <Box width={"100%"} height={"100vh"} />
+      <Event />
       <Workshop />
       <FAQ />
     </Box>
