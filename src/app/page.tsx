175ab9a3bf2f3fbaefa86d37abaee56898ac8539
Matthew Haryanto <matthewanh@hotmail.com>
2024-12-29 12:19:02 -0800

    feat: add upcoming events section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Landing from "@/components/home/Landing";
 import Workshop from "@/components/home/Workshop";
 import Event from "@/components/home/Event";
 import { Box } from "@chakra-ui/react";
+import UpcomingEvent from "@/components/home/Upcoming";
 
 export default function Home() {
   return (
     <Box position={"relative"}>
       <Landing />
       <Event />
+      <UpcomingEvent />
       <Workshop />
       <FAQ />
     </Box>
